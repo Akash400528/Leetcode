f856dfd60dc7341fa1c7d81e1acdcb0a9991a62e
Akash400528 <akashmohantn47@gmail.com>
2026-08-25 19:51:53 +0530

    Time: 2 ms (10.52%), Space: 42.9 MB (51.55%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,7 +1,21 @@
 class Solution {
     public String toLowerCase(String s) {
-        String m=s.toLowerCase();
-        return m;
-        
+        Map<Character, Character> map = new HashMap<>();
+
+        for (int i = 0; i < 26; i++) {
+            map.put((char)('A' + i), (char)('a' + i));
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (map.containsKey(ch)) {
+                ans.append(map.get(ch));
+            } else {
+                ans.append(ch);
+            }
+        }
+
+        return ans.toString();
     }
 }
