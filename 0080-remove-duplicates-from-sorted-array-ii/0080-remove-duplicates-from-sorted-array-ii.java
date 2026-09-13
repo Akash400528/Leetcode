e50b43c30786838e7de78421e4a90b0a131cf700
Akash400528 <akashmohantn47@gmail.com>
2026-09-13 20:24:35 +0530

    Time: 2 ms (6.3%), Space: 48.3 MB (94.12%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,41 @@
+class Solution
+{ 
+    public int removeDuplicates(int[] nums) 
+    {
+        ArrayList<Integer> list=new ArrayList<>();
+        int c=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            
+            if(nums[i-1]==nums[i])
+            {
+                c++;
+            }
+            else{
+                if(c>=2)
+                {
+                    for(int l=0;l<2;l++)
+                    {
+                    list.add(nums[i-1]);
+                    }
+                    c=1;
+                }
+                else{
+                    list.add(nums[i-1]);
+                }
+            }}
+            if(c<2)
+            {
+                list.add(nums[nums.length-1]);
+
+            }else{
+              list.add(nums[nums.length-1]);
+              list.add(nums[nums.length-1]);
+            }
+       
+            for(int i=0;i<list.size();i++)
+            {
+                nums[i]=list.get(i);
+            }
+            return list.size();}}
+               
